829b54996fdb5693e4bfedc4732331f3851b5059
kd20072348-arch <kd2007.2348@gmail.com>
2026-03-28 15:53:36 +0530

    Coded UC7

--- src/main/java/com/helloapp/HelloApp.java
+++ src/main/java/com/helloapp/HelloApp.java
@@ -5,13 +5,7 @@ public class HelloApp {
         if (args.length == 0) {
             System.out.println("Hello, World!");
         } else {
-            StringBuilder nameBuilder = new StringBuilder();
-            for (String name : args) {
-                nameBuilder.append(name);
-                nameBuilder.append(", ");
-            }
-            String names = nameBuilder.substring(0, nameBuilder.length() - 2);
-            System.out.println("Hello, " + names + "!");
+            System.out.println("Hello, " + String.join(", ", args) + "!");
         }
     }
 }
