fb05707ed4c7990329abe5b0721007f5bdde6592
kd20072348-arch <kd2007.2348@gmail.com>
2026-03-28 15:38:10 +0530

    Coded UC4

--- src/main/java/com/helloapp/HelloApp.java
+++ src/main/java/com/helloapp/HelloApp.java
@@ -2,7 +2,7 @@ package com.helloapp;
 
 public class HelloApp {
     public static void main(String[] args) {
-        String name = (args.length > 0) ? args[0] : "World";
+        String name = (args.length > 0) ? String.join(", ", args) : "World";
         System.out.println("Hello, " + name + "!");
     }
 }
